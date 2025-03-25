fb1424d88a346b6afd7feccba46dd1cfbc299a71
Raza Awan <rzqutubawan@gmail.com>
2025-03-26 00:54:12 +0500

    Add navigation constants, implement smooth scrolling function, and update Header component formatting

---
+++ frontend/src/app/constants/index.js
@@ -0,0 +1,27 @@
+export const navLinks = [
+    {
+      id: "home",
+      name: "Home",
+      href: "#home",
+    },
+    {
+      id: "about",
+      name: "About",
+      href: "#about",
+    },
+    {
+      id: "work",
+      name: "Work",
+      href: "#work",
+    },
+    {
+      id: "skills",
+      name: "Skills",
+      href: "#skills",
+    },
+    {
+      id: "contact",
+      name: "Contact",
+      href: "#contact",
+    },
+  ];
