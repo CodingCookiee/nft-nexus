d9d2eb550ac5f48bb1afa12aa922913413dcfb2b
Raza Awan <rzqutubawan@gmail.com>
2025-04-15 16:32:45 +0500

    feat: enhance Header component and update config for multiple blockchain support

--- frontend/src/app/components/layout/Header.jsx
+++ frontend/src/app/components/layout/Header.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React from "react";
+import { Text } from "../ui/common";
+import { Button } from "../ui/common";
+import Link from 'next/link';
 
-const Header = () => {
+export default function Header() {
   return (
-    <div>
-      Header
-    </div>
+    <header className="w-full  px-5 py-5 flex justify-between items-center ">
+      <div className="flex items-center">
+        <img
+          src="/app/favicon.png"
+          alt="NFT Nexus Logo"
+          className="h-10 w-10 mr-2"
+        />
+        <Text
+          variant="h2"
+          weight="semibold"
+          className="bg-gradient-to-r from-[#4B0082] to-[#AAA9CF] bg-clip-text text-transparent"
+        >
+          NFT Nexus
+        </Text>
+      </div>
+      <div className="flex items-center justify-center px-5">
+        <Link href="/">
+          <Button variant="default" >
+            Home
+          </Button>
+        </Link>
+      </div>
+    </header>
   );
 }
-
-export default Header;
--- frontend/src/app/utils/config.js
+++ frontend/src/app/utils/config.js
@@ -1,25 +1,80 @@
-import { createConfig, http } from 'wagmi';
-import { mainnet, sepolia } from 'wagmi/chains'; 
-import { getDefaultConfig } from 'connectkit'
+import { createConfig, http } from "wagmi";
+import {
+  arbitrum,
+  base,
+  mainnet,
+  optimism,
+  polygon,
+  sepolia,
+} from "wagmi/chains";
+import { getDefaultConfig } from "connectkit";
+import {
+  coinbaseWallet,
+  injected,
+  metaMask,
+  walletConnect,
+} from "wagmi/connectors";
 
-
-
-const chainId = parseInt(process.env.NEXT_CHAIN_ID || '11155111');
+const chainId = parseInt(process.env.NEXT_CHAIN_ID || "11155111");
 const activeChain = chainId === 1 ? mainnet : sepolia;
 
-export const config = createConfig(getDefaultConfig({
-    chains: [activeChain],
+export const config = createConfig(
+  getDefaultConfig({
+    chains: [arbitrum, base, mainnet, optimism, polygon, sepolia],
+    connectors: [
+      coinbaseWallet({
+        qrcode: true,
+        appName: "NFT Nexus",
+        appLogoUrl: "/app/favicon.png",
+        darkMode: true,
+      }),
+      //   injected(
+      //     {
+      //     target() {
+      //       return {
+      //         id: "windowProvider",
+      //         name: "Window Provider",
+      //         provider: window.ethereum,
+      //       };
+      //     },
+      //   }
+      // ),
+      //   metaMask({
+      //     headless: true,
+      //     logging: { developerMode: true, sdk: true },
+      //     showQrModal: true,
+      //     dappMetadata: {
+      //       name: "NFT Nexus",
+      //       description: "NFT Nexus - Your NFT Dashboard & Marketplace",
+      //       url: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3001",
+      //       iconUrl: "app/favicon.png",
+      //     },
+      //   }),
+      walletConnect({
+        customStoragePrefix: "wagmi",
+        projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID,
+        showQrModal: false,
+        metaData: {
+          name: "NFT Nexus",
+          description: "NFT Nexus - Your NFT Dashboard & Marketplace",
+          url: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3001",
+        },
+        qrModalOptions: {
+          themeMode: "dark",
+        },
+      }),
+    ],
     transports: {
-        [mainnet.id]: http(
+      [mainnet.id]: http(
         `https://eth-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_ID}`
-        ),
-        [sepolia.id]: http(
+      ),
+      [sepolia.id]: http(
         process.env.NEXT_RPC_URL || "https://eth-sepolia.public.blastapi.io"
-        ),
+      ),
     },
-    walletConnectProjectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID, 
-    appName: 'NFT Nexus',
+    appName: "NFT Nexus",
     appDescription: "NFT Nexus - Your NFT Dashboard & Marketplace",
-    appUrl: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3001", 
+    appUrl: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3001",
     appIcon: "/app/favicon.png",
-}));
+  })
+);
